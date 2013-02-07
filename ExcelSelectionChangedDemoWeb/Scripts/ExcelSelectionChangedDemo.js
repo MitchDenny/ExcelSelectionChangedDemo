038e7db379f18f0a1becf74f7844eabe9b5bbb29
Mitch Denny <mitch@mitchdenny.com>
2013-02-07 13:18:21 +1100

    Initial commit.

---
+++ ExcelSelectionChangedDemoWeb/Scripts/ExcelSelectionChangedDemo.js
@@ -0,0 +1,37 @@
+﻿function PreviousSelection(status, jsonText, sequence) {
+    var self = this;
+    this.status = ko.observable(status);
+    this.jsonText = ko.observable(jsonText);
+    this.sequence = ko.observable(sequence);
+}
+
+function MainViewModel() {
+    var self = this;
+    this.previousSelections = ko.observableArray();
+}
+
+Office.initialize = function (reason) {
+    $(document).ready(function () {
+
+        var history = document.getElementById('History');
+        viewModel = new MainViewModel();
+        ko.applyBindings(viewModel, history);
+
+        Office.context.document.addHandlerAsync(
+            Office.EventType.DocumentSelectionChanged,
+            SelectionChanged
+            );
+    });
+};
+
+function SelectionChanged(args) {
+    args.document.getSelectedDataAsync(Office.CoercionType.Matrix, {}, DataSelected);
+}
+
+function DataSelected(result) {
+    var sequence = viewModel.previousSelections().length + 1;
+    var status = result.status;
+    var jsonText = JSON.stringify(result, {}, ' ');
+    var previousSelection = new PreviousSelection(status, jsonText, sequence);
+    viewModel.previousSelections.unshift(previousSelection);
+}
